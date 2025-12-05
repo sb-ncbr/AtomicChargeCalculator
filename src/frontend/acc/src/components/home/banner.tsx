0c0b4e6a19d8f319f1f8f27ae3ccdfa9e121e44b
Tomáš Raček <tom@krab1k.net>
2025-12-05 08:39:25 +0100

    Simplify homepage banner

--- src/frontend/acc/src/components/home/banner.tsx
+++ src/frontend/acc/src/components/home/banner.tsx
@@ -1,23 +1,10 @@
-import BannerImage from "@acc/assets/images/1RLB.png";
-
 export const Banner = () => {
   return (
     <div className="w-11/12 md:w-4/5 max-w-content mx-auto">
-      <div className="flex relative max-w-[750px] mx-auto">
-        <div className="my-16 p-2 bg-black bg-opacity-10 w-fit z-30">
+      <div className="flex mt-12 relative mx-auto">
           <h1 className="font-muni font-bold text-primary text-4xl md:text-6xl">
-            <span className="block bg-white w-fit">Atomic</span>
-            <span className="block my-2 bg-white w-fit">Charge</span>
-            <span className="block bg-white w-fit">Calculator III-dev</span>
+            <span className="block bg-white w-fit">Atomic Charge Calculator III</span>
           </h1>
-        </div>
-        <img
-          src={BannerImage}
-          alt="1RLB"
-          className="absolute min-w-[350px] sm:right-0"
-          width={500}
-          height={250}
-        />
       </div>
     </div>
   );
